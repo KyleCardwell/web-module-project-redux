d2b48e8e7eba28f665ebdac0ae54d8f8ffffe592
Kyle <kyle.e.cardwell@gmail.com>
2021-07-24 10:41:19 -0600

    completed addMovie and deleteMovie code

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -3,14 +3,21 @@ import movies from './../data.js';
 
 const initialState = {
     movies: movies,
-    appTitle: "IMDB Movie Database"
+    appTitle: "IMDb Movie Database"
 }
 
-const reducer = (state, action) => {
+const reducer = (state = initialState, action) => {
     switch(action.type) {
         case DELETE_MOVIE:
             return {
                 movies: state.movies.filter(item=>(action.payload !== item.id))
+            }
+        case ADD_MOVIE:
+            const movieToAdd = {...action.payload, id: Math.floor(Math.random() *1000*action.payload.lenth)}
+            console.log(movieToAdd)
+            return {
+                ...state,
+                movies: [...state.movies, movieToAdd]
             }
         default:
             return state;
